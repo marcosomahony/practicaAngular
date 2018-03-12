460a7a3fd2ae465a6dae01cd49ce287327983825
Marcos O'Mahony <marcos.omahony.alonso@alumnos.upm.es>
2018-03-12 13:40:48 +0100

    home esta ya, menos el video

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, HomeContentComponent],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
