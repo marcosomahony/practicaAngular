b73a80a36821330304ce339019183f3c17a8a9a8
CBSanta <catalin.beniamin@babel.es>
2018-03-12 13:50:55 +0100

    ahora si que si arreglado 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', loadChildren: './home/home.module#HomeModule' },
   { path: 'about', loadChildren: './about/about.module#AboutModule' },
   { path: 'blog', loadChildren: './blog/blog.module#BlogModule' },
   { path: '', pathMatch: 'full', redirectTo: 'home' }
