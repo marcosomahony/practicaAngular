44e9d5d2aea2e7e16f78a8894576fae6fbf7649c
Pablo Psichalis Ruiz <pablo.psrz@gmail.com>
2018-03-13 13:25:11 +0100

    FormContacto parcialmente terminado

---
+++ src/app/models/contacto.model.ts
@@ -0,0 +1,18 @@
+export interface ContactoIf {
+    nombre: string;
+    email: string;
+    sugerencias: string;
+    newsletter: boolean;
+    terminos: boolean;
+}
+
+/* export class Contacto implements ContactoIf {
+    constructor(
+        public nombre: string,
+        public email: string,
+        public sugerencias: string,
+        public newsletter: boolean,
+        public terminos: boolean
+    ) { }
+}
+ */
